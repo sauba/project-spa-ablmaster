fa2a29ef130ba96a9b843a927398698860b2dfa2
Thiago Queiroz <thiagojqueiroz@outlook.com>
2026-05-01 22:36:56 -0300

    Update: Site em contrucao v7

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -12,17 +12,17 @@ export default function About() {
         min-h-screen
         flex
         flex-col
-        justify-center
+        justify-evenly
         items-center
-      text-gray-50
-
+        text-gray-50
       `}
       id="about"
     >
-      <Image src={logoablmaster} alt="" className={`position:relative w-4/12 mx-auto z-10`} />
+      <div className={`w-full min-h-screen bg-black/50`}>
+        <Image src={logoablmaster} alt="" className={`position:relative w-4/12 h-auto mx-auto z-10`} />
 
-      <p
-        className={`
+        <p
+          className={`
             px-2
             py-10
             2xl:px-8
@@ -34,10 +34,12 @@ export default function About() {
             z-10
             text-zinc-100
           `}
-      >
-        ABLM - Associação Brasileira de Longboard Master, uma entidade dedicada à promoção, desenvolvimento e valorização do surfe na modalidade longboard, com foco especial nos atletas da categoria master +35 anos.
-        A AssociaçÃo é filiada ao CBEM - Comitê Brasileiro do Esporte Master órgão representativo do esporte master nacional, o qual, por sua vez, está vinculado à entidade internacional , principal WMS - World Masters Sport. a inserção da ABLMaster , consolidando  no cenário esportivo de alcance global. Nosso objetivo é fortalecer o esporte como ferramenta de inclusão social, qualidade de vida, turismo esportivo e valorização cultural; promover eventos, treinamentos e ações sociais, em parceria com o poder público e a iniciativa privada.
-      </p>
+        >
+          ABLM - Associação Brasileira de Longboard Master, uma entidade dedicada à promoção, desenvolvimento e valorização do surfe na modalidade longboard, com foco especial nos atletas da categoria master +35 anos.
+          A AssociaçÃo é filiada ao CBEM - Comitê Brasileiro do Esporte Master órgão representativo do esporte master nacional, o qual, por sua vez, está vinculado à entidade internacional , principal WMS - World Masters Sport. a inserção da ABLMaster , consolidando  no cenário esportivo de alcance global. Nosso objetivo é fortalecer o esporte como ferramenta de inclusão social, qualidade de vida, turismo esportivo e valorização cultural; promover eventos, treinamentos e ações sociais, em parceria com o poder público e a iniciativa privada.
+        </p>
+      </div>
+
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,6 @@ export default function Home() {
       <Header />
       <UnderConstruction />
       <About />
-      <Missao />
-      <Filiados />
       <Footer />
     </main>
   )
